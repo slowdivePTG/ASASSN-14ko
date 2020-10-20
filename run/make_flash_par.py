70c1c124aecd4ab8716c63f0c6398cd20fc0f9c4
slowdivePTG <1600012139@pku.edu.cn>
2020-10-20 15:07:35 +0800

    make physical OrbEcc

--- run/make_flash_par.py
+++ run/make_flash_par.py
@@ -13,8 +13,10 @@ def make_flash_par(num):
     Mbh = 1e6 * Msun
     P = 110 * day
     a = ((gravitational_constant * Mbh / 4 / np.pi**2) * P**2)**(1 / 3)
-    e = 0.99999
-    r_peri = (a * (1 - e)).in_cgs()
+    rT = (R * (Mbh / M)**(1 / 3)).in_cgs()
+    beta = 0.6
+    r_peri = rT / beta
+    e = 1 - r_peri / a
     print(a.in_units('AU'), r_peri.in_units('AU'))
 
     with open('./flash.par', 'r') as f:
@@ -37,6 +39,7 @@ def make_flash_par(num):
         lines[78] = 'sim_tRelax = {:.6e}   # 5 tdyn\n'.format(float(tdyn) * 5)
 
         lines[62] = 'sim_parentPeri = {:.8e}\n'.format(float(r_peri))
+        lines[67] = 'sim_orbEcc    = {:.8e}\n'.format(float(e))
         lines[167] = 'sink_softening_radius = {:.8e} # ~1/2 rp\n'.format(float(r_peri)/2)
 
     with open('./flash.par', 'w') as f:
