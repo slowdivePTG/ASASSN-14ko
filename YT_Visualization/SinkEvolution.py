1bf624cf5babdacabe46f5ca303b946e3889860e
slowdivePTG <1600012139@pku.edu.cn>
2020-12-09 17:37:47 +0800

    Orbital period double check

--- YT_Visualization/SinkEvolution.py
+++ YT_Visualization/SinkEvolution.py
@@ -5,6 +5,14 @@ import yt.units as u
 import yt
 import os
 
+part_tag = 0
+time = 1
+posx, posy, posz = 2, 3, 4
+velx, vely, velz = 5, 6, 7
+accelx, accely, accelz = 8, 9, 10
+anglx, angly, anglz = 11, 12, 13
+mass, mdot, ptime = 14, 15, 16
+
 
 class SinkEvol:
     def __init__(self, DIR, runs, force, clean):
@@ -25,7 +33,7 @@ class SinkEvol:
 
         exist = False
         for i in dirs:
-            temp = '{}_extras.dat'.format(runs)
+            temp = '{}_pruned_sinks_evol.dat'.format(runs)
             if temp in i:
                 exist = True
                 break
@@ -81,12 +89,14 @@ class SinkEvol:
             df.to_csv(self.output,
                       sep=' ',
                       index=False,
-                      header=True)
-        df = pd.read_table(self.output, delim_whitespace=True)
+                      header=False)
+        df = np.loadtxt(self.output)
+        part_tag_bh = df[0, part_tag] if df[0, mass
+                                            ] > df[1, mass] else df[1, part_tag]
 
-        self.star = df[1::2]
-        self.bh = df[0::2]
-        self.T = np.array(self.star['[01]time'], dtype='f4')
+        self.bh = df[df[:, part_tag] == part_tag_bh, :]
+        self.star = df[df[:, part_tag] != part_tag_bh, :]
+        self.T = self.star[:, time]
         os.system('rm ../../TDE_plot/sinks_evol.dat')
         self.runs = runs
 
@@ -96,36 +106,45 @@ class SinkEvol:
         G = u.gravitational_constant
         rho = self.ms / (self.rs**3)
         self.tdyn = 1 / np.sqrt(G * rho)
-        print('Tdyn = {:.2f} s'.format(self.tdyn))
+        print('Tdyn = {:.2f}'.format(self.tdyn))
 
-    def orbit(self):
-        plt.figure(figsize=(12, 8))
-        plt.scatter(
-            self.bh['[02]posx'][::200] * u.cm.in_units('AU'),
-            self.bh['[03]posy'][::200] * u.cm.in_units('AU'))
-        plt.scatter(
-            self.star['[02]posx'][::200] * u.cm.in_units('AU'),
-            self.star['[03]posy'][::200] * u.cm.in_units('AU'),
-            s=self.star['[01]time'][::200]**2 * 1e-7)
-        ntdyn = 1
-        arg = np.array([], dtype='i4')
-        if self.tdyn > 0:
-            while self.tdyn * ntdyn < self.T[-1]:
-                arg = np.append(arg, np.argwhere(
-                    self.T > self.tdyn * ntdyn).flatten()[0])
-                ntdyn += 1
-            relaxx = np.array(self.star['[02]posx'])[arg] * u.cm.in_units('AU')
-            relaxy = np.array(self.star['[03]posy'])[arg] * u.cm.in_units('AU')
-            plt.scatter(relaxx, relaxy, marker='+', s=50, color='k')
+    def orbit(self, ax=None):
+        if ax == None:
+            plt.figure(figsize=(12, 8))
+        x = ((self.star[:, posx] - self.bh[:, posx]) * u.cm).in_units('AU')
+        y = ((self.star[:, posy] - self.bh[:, posy]) * u.cm).in_units('AU')
+        vel2 = 0
+        for ax in [velx, vely, velz]:
+            vel2 += ((self.star[:, ax] - self.bh[:, ax])
+                     * u.cm / u.s)**2
+        r2 = 0
+        for ax in [posx, posy, posz]:
+            r2 += ((self.star[:, ax] - self.bh[:, ax])
+                   * u.cm)**2
+        gpot = u.gravitational_constant * self.bh[:, mass] * u.gram
+        E = -gpot / np.sqrt(r2) + 0.5 * vel2
+        P = np.pi / np.sqrt(2) * gpot / (-E)**(3 / 2)
+        print(np.mean(P).in_units('day'), np.std(P).in_units('day'))
+        pmean = np.mean(P).in_units('day').v
+        plt.plot(x[::200], y[::200], label='{:.0f}'.format(pmean))
+
+        '''ntdyn = 1
+            arg = np.array([], dtype='i4')
+            if self.tdyn > 0:
+                while self.tdyn * ntdyn < self.T[-1]:
+                    arg = np.append(arg, np.argwhere(
+                        self.T > self.tdyn * ntdyn).flatten()[0])
+                    ntdyn += 1
+                plt.scatter(x[arg], y[arg], marker='+', s=50, color='k')'''
         plt.xlabel('X (AU)', fontsize=20)
         plt.ylabel('Y (AU)', fontsize=20)
         plt.axis('equal')
         plt.tight_layout()
-        plt.show()
+        # plt.show()
 
     def delta_t(self):
         dT = self.T[1:] - self.T[:-1]
-        #print(dT.min(), dT.max(), np.median(dT))
+        # print(dT.min(), dT.max(), np.median(dT))
         f, ax = plt.subplots(2, 1, figsize=(12, 12))
         ax[0].hist(dT, bins=100)
         ax[1].plot(range(len(dT)), dT)
@@ -143,14 +162,17 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-d', dest='DIR',
                     help='Directory', default='ptgcliu')
 parser.add_argument('--run', '-r', dest='runs',
-                    help='Specific runs directory', default='m0.8_p1_b0.6')
+                    help='Specific runs directory', default=['m0.8_p1_b0.6'], nargs='+')
 parser.add_argument('--force', '-f', dest='force',
                     help='Force to rewrite pruned datafile', default=False, action='store_true')
 parser.add_argument('--clean', '-c', dest='clean',
                     help='Clean pruned datafiles', default=False, action='store_true')
 args = parser.parse_args()
 
-test = SinkEvol(DIR=args.DIR, runs=args.runs,
-                force=args.force, clean=args.clean)
-test.orbit()
-test.delta_t()
+f, ax = plt.subplots(figsize=(8, 8))
+for run in args.runs:
+    test = SinkEvol(DIR=args.DIR, runs=run,
+                    force=args.force, clean=args.clean)
+    test.orbit(ax=ax)
+f.legend()
+plt.show()
